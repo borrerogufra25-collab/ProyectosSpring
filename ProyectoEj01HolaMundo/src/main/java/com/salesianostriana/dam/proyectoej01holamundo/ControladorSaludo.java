10c2b748440983a6e8be197da82f74da341f9857
Francisco Borrero <borrero.gufra25@triana.salesianos.edu>
2026-04-30 10:45:41 +0200

    Cosas

--- ProyectoEj01HolaMundo/src/main/java/com/salesianostriana/dam/proyectoej01holamundo/ControladorSaludo.java
+++ ProyectoEj01HolaMundo/src/main/java/com/salesianostriana/dam/proyectoej01holamundo/ControladorSaludo.java
@@ -1,8 +1,11 @@
 package com.salesianostriana.dam.proyectoej01holamundo;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //Un controller es el que recibe una petición,
 //hace algo o se lo manda al que sea, y luego lo manda a pintar 
@@ -24,10 +27,24 @@ public class ControladorSaludo {
 	@GetMapping("/saludo2")
 	public String welcome2(Model model) {
 
-		model.addAttribute("persona", new Persona("Fran", "Borrero Guerrero"));
+		model.addAttribute("persona", new Persona("Fran", "Borrero Guerrero", 0, null));
 
 		return "SaludoPersonalizado";
 
+	}
+
+	@GetMapping({ "/", "welcome" }) // Esto sirve para hacer que con el mismo controller se pueda llamar dos veces
+	public String welcome(@RequestParam(name = "nombre", required = false, defaultValue = "Mundo") String nombre,
+			Model model) {
+
+		model.addAttribute("nombre", nombre);
+		return "index";
+
+	}
+
+	public String welcomePotente(Model model) {
+		model.addAttribute("personaPotente", new Persona("Fran", "Borrero", 123.534252345, LocalDate.of(1998, 05, 11)));
+		return "Saludo tocho";
 	}
 
 }
--- ProyectoEj01HolaMundo/src/main/java/com/salesianostriana/dam/proyectoej01holamundo/Persona.java
+++ ProyectoEj01HolaMundo/src/main/java/com/salesianostriana/dam/proyectoej01holamundo/Persona.java
@@ -1,9 +1,12 @@
 package com.salesianostriana.dam.proyectoej01holamundo;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//Lombok hace ahorrarte escribir los contructores, getter and setters, etc.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +14,7 @@ public class Persona {
 
 	private String nombre;
 	private String apellidos;
+	private double notaMedia;
+	private LocalDate fecha;
 
 }
