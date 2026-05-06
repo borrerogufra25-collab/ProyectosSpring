1f991b4797d7ba763dff8401fa15610e6d6ef174
Francisco Borrero <borrero.gufra25@triana.salesianos.edu>
2026-05-06 14:26:56 +0200

    vgh

--- EjemploWebData/src/main/java/com/salesianostriana/dam/EjemploWebData/repos/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.salesianostriana.dam.EjemploWebData.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.salesianostriana.dam.EjemploWebData.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
