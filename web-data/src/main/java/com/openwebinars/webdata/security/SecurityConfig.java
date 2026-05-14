363474d405198aa7e08c725e210518762a05e223
Francisco Borrero <borrero.gufra25@triana.salesianos.edu>
2026-05-14 13:06:46 +0200

    erth

--- web-data/src/main/java/com/openwebinars/webdata/security/SecurityConfig.java
+++ web-data/src/main/java/com/openwebinars/webdata/security/SecurityConfig.java
@@ -18,43 +18,49 @@ import org.springframework.security.web.savedrequest.RequestCache;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		UserDetails user = User.builder()
-				.username("admin")
-				.password("{noop}admin")
-				.roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(user);
-	}
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests((authz) -> authz.requestMatchers("/producto/**")
-				.authenticated()
-				.anyRequest()
-				.permitAll())
-				.requestCache(cache -> {
-					HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-					requestCache.setMatchingRequestParameterName(null);
-					cache.requestCache(requestCache);
-				})
-				.formLogin(form -> form.loginPage("/login")
-						.defaultSuccessUrl("/producto/list", true)
-						.permitAll())
-				/*
-				 * .logout(logout -> logout .logoutUrl("/logout") .permitAll() );
-				 */
-				.logout(Customizer.withDefaults());
 
-		// Añadimos esto para poder acceder a la consola de H2
-		// con Spring Security habilitado.
-		http.csrf((csrf) -> {
-			csrf.ignoringRequestMatchers("/h2/**");
-		});
-		http.headers((headers) -> headers.frameOptions((opts) -> opts.disable()));
-		return http.build();
-	}
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/producto/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .requestCache(cache -> {
+                    HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+                    requestCache.setMatchingRequestParameterName(null);
+                    cache.requestCache(requestCache);
+                })
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/producto/list", true)
+                        .permitAll()
+                )
+                /*.logout(logout -> logout
+                                .logoutUrl("/logout")
+                                .permitAll()
+                );*/
+                .logout(Customizer.withDefaults());
+
+        // Añadimos esto para poder acceder a la consola de H2
+        // con Spring Security habilitado.
+        http.csrf((csrf) -> {
+            csrf.ignoringRequestMatchers("/h2/**");
+        });
+        http.headers((headers) ->
+                headers.frameOptions((opts) -> opts.disable()));
+        return http.build();
+    }
 
 }
