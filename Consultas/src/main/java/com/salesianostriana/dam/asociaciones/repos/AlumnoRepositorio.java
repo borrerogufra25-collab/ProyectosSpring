283b3a38d98b1f317a639366d006f5d0495727c5
Francisco Borrero <borrero.gufra25@triana.salesianos.edu>
2026-05-25 08:16:43 +0200

    Update AlumnoRepositorio.java

--- Consultas/src/main/java/com/salesianostriana/dam/asociaciones/repos/AlumnoRepositorio.java
+++ Consultas/src/main/java/com/salesianostriana/dam/asociaciones/repos/AlumnoRepositorio.java
@@ -9,14 +9,13 @@ import org.springframework.data.repository.query.Param;
 
 import com.salesianostriana.dam.asociaciones.model.Alumno;
 
-public interface AlumnoRepositorio
-	extends JpaRepository<Alumno, Long>{
-	
+public interface AlumnoRepositorio extends JpaRepository<Alumno, Long> {
+
 	List<Alumno> findByApellidosContainsIgnoreCase(String apellidos);
-	
-	List<Alumno> findByApellidosContainsIgnoreCaseOrNombreContainsIgnoreCaseOrEmailContainsIgnoreCaseOrderByApellidosDesc(String apellidos, String nombre, String email);
-	
-	
+
+	List<Alumno> findByApellidosContainsIgnoreCaseOrNombreContainsIgnoreCaseOrEmailContainsIgnoreCaseOrderByApellidosDesc(
+			String apellidos, String nombre, String email);
+
 	@Query("""
 			select a
 			from Alumno a
@@ -24,7 +23,7 @@ public interface AlumnoRepositorio
 				or a.nombre = ?2
 			""")
 	List<Alumno> buscarPorTodo(String ape, String nom);
-	
+
 	@Query("""
 			select a
 			from Alumno a
@@ -32,19 +31,15 @@ public interface AlumnoRepositorio
 				or a.nombre = :nom
 			""")
 	List<Alumno> buscarPorTodov2(String ape, String nom);
-	
+
 	@Query("""
 			select a
 			from Alumno a
 			where a.apellidos = :ape
 				or a.nombre = :nom
 			""")
-	List<Alumno> buscarPorTodov3(
-			@Param("ape") String apellidos, 
-			@Param("nom") String nombre);
-	
-	
-	
+	List<Alumno> buscarPorTodov3(@Param("ape") String apellidos, @Param("nom") String nombre);
+
 	// SELECT * FROM ALUMNO WHERE APELLIDOS = '..';
 
 }
